3a0fdada98c98d0baf2bc93dafaa8f6a60d785d8
WAE <prangwalai.li@rmuti.ac.th>
2020-07-21 22:56:16 +0700

    12. Task: Add click handlers to the buttons 1

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
